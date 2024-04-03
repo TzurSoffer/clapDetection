5c029ab43a83b5c509ec5a9349103df3f6be8e5e
Tzur <103438808+RandomTheFirst@users.noreply.github.com>
2024-04-02 21:00:27 -0700

    added pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "clap-detector",
+    version = "2.0.2",
+    author = "Tzur Soffer",
+    author_email = "tzur.soffer@gmail.com",
+    description = "A clap detector that can detect claps in patterns of single, double, etc.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/TzurSoffer/clapDetection",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src")
+)
