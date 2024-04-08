24d2ef9822dafc1178bff33e2886e95b5dd6f9d5
Tzur <103438808+RandomTheFirst@users.noreply.github.com>
2024-04-07 18:41:42 -0700

    fixed pip dependencies issue.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name = "clap-detector",
-    version = "2.0.3",
+    version = "2.0.4",
     author = "Tzur Soffer",
     author_email = "tzur.soffer@gmail.com",
     description = "A clap detector that can detect claps in patterns of single, double, etc.",
@@ -16,6 +16,11 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "pyaudio"
     ],
     package_dir = {"": "src"},
     packages = setuptools.find_packages(where="src")
